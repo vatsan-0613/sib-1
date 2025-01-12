98005db597457f3664b37144b66a4917c67a0aa7
vatsan-0613 <srivatsan050@gmail.com>
2025-01-12 15:17:36 +0530

    minor changes

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,43 +5,55 @@ const Products = () => {
   return (
     <>
     <section id="products">
-        <div className="container">
+        <div className="container d-flex justify-content-center">
             <div className="row">
                 <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
-                <div className="col-12  col-sm-6">
+                <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
-                <div className="col-12 col=sm-6 mt-5">
+                <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-6 mt-5">
+                <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-6 mt-5">
+                <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-6 mt-5">
+                <div className="col-12 col-sm-6">
+                    <div className="d-flex flex-column ms-auto">
                     <img src="assets/product-image.png" alt="" className="product-img" />
                     <h4 className="fw-semibold mt-3 mb-2">Product name</h4>
                     <p className="product-desc">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
                     <p className="blue-txt read-more">Read More <img src="/assets/blue-arrow.png" alt="" className="blue-arrow ms-2"/></p>
+                    </div>
                 </div>
             </div>
         </div>
